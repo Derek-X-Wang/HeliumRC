3a0073abe7b4b9c1b690ba3d8636913734c22b00
Derek Wang <derekxinzhewang@gmail.com>
2017-01-02 21:56:01 -0800

    Initial Commit

---
+++ HeliumRC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HeliumRC
+//
+//  Created by Xinzhe Wang on 1/2/17.
+//  Copyright © 2017 IntBridge. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
