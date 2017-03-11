0f5246bd6a240a4e73dec4d9b4c88cb063245864
Derek Wang <derekxinzhewang@gmail.com>
2017-03-11 04:30:43 -0800

    New -> constants and utils

---
+++ HeliumRC/constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  HeliumRC
+//
+//  Created by Xinzhe Wang on 3/11/17.
+//  Copyright © 2017 IntBridge. All rights reserved.
+//
+
+import Foundation
---
+++ HeliumRC/utils.swift
@@ -0,0 +1,9 @@
+//
+//  Utils.swift
+//  HeliumRC
+//
+//  Created by Xinzhe Wang on 3/11/17.
+//  Copyright © 2017 IntBridge. All rights reserved.
+//
+
+import Foundation
